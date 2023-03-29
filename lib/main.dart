103e13a5e9c5e0a6019f4178d996f7d8db2cbf28
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2023-03-29 18:54:18 +0900

    feat: edit

--- lib/main.dart
+++ lib/main.dart
@@ -50,9 +50,31 @@ class _TodoListPageState extends State<TodoListPage> {
           return Card(
             child: ListTile(
               title: Text(todoList[index]),
-              trailing: IconButton(
-                icon: const Icon(Icons.delete),
-                onPressed: () => _removeTodoItem(index),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.edit),
+                    onPressed: () async {
+                      final editedText = await Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) {
+                          return TodoEditPage(
+                            initialText: todoList[index]
+                          );
+                        }),
+                      );
+                      if (editedText != null) {
+                        setState(() {
+                          todoList[index] = editedText;
+                        });
+                      }
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () => _removeTodoItem(index),
+                  ),
+                ],
               ),
             ),
           );
@@ -116,6 +138,72 @@ class _TodoAddPageState extends State<TodoAddPage> {
                   Navigator.of(context).pop(_text);
                 },
                 child: const Text('リスト追加', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            const SizedBox(height: 8),
+            SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('キャンセル'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TodoEditPage extends StatefulWidget {
+  final String initialText;
+
+  const TodoEditPage({super.key, required this.initialText});
+
+  @override
+  _TodoEditPageState createState() => _TodoEditPageState();
+}
+
+class _TodoEditPageState extends State<TodoEditPage> {
+  late String _text;
+
+  @override
+  void initState() {
+    super.initState();
+    _text = widget.initialText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('リスト編集'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(64),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(_text, style: const TextStyle(color: Colors.blue)),
+            const SizedBox(height: 8),
+            TextField(
+              onChanged: (String value) {
+                setState(() {
+                  _text = value;
+                });
+              },
+              controller: TextEditingController(text: _text),
+            ),
+            const SizedBox(height: 8),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop(_text);
+                },
+                child: const Text('リスト編集', style: TextStyle(color: Colors.white)),
               ),
             ),
             const SizedBox(height: 8),
